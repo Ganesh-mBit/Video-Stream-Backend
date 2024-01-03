dc299396a8d779193ee6304bcd1200725a49116b
Ganesh M <ganesh.mittha@binarytouch.com>
2024-01-03 19:23:20 +0530

    cloudinary hardcoded api key & secret removed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,16 +2,10 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 // Code not workinh when pass environment variables
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
 cloudinary.config({
-  cloud_name: "demugmjou",
-  api_key: "548425971341867",
-  api_secret: "KEtOJ0VC4uCpAdQTRgIOvUBvTTY",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (filePath) => {
